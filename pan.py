8115f669268c3e0035230953b46b24966af5dcc4
hallotype <thomjanssen128@gmail.com>
2021-05-12 11:48:40 +0200

    more arguments

--- pan.py
+++ pan.py
@@ -38,24 +38,33 @@ def font2ufo(fontpath):
   return ufo
 
 def run():
-  parser = argparse.ArgumentParser(description='''Font filter PAN''',
-                                     )
-  parser.add_argument('-f', '--fontfile', type=str,
-                        required=True, help='otf or ttf font')
+  parser = argparse.ArgumentParser(description='''Font filter PAN''')
+
+  # not pan specific                                     )
   parser.add_argument('-c', '--characters', default=string.ascii_letters, type=str,
                         help="Characters...")
+  parser.add_argument('-f', '--fontfile', type=str,
+                        required=True, help='otf or ttf font')
+  
+  # pan
   parser.add_argument('-steps', default=30, type=int,
-                        help="Steps")
+                        help="Interval of the slicer, in fontUnits")
   parser.add_argument('-angle', default=15, type=int,
                         help="Angle in degrees.")
   parser.add_argument('-t', '--thickness', default=30, type=int,
                         help="Thickness in font units.")  
-  parser.add_argument('-shape', default="plainRect", type=str,
-                        help="plainRect, plainRound, hex, tri, diamondPoint") 
   parser.add_argument('-noCounters',default=False, type=bool,
                         const=True, nargs="?", help="Ignore inner contours.")
+  parser.add_argument('-noSmall', default=30, type=int, help='No segments under this value.')
+  parser.add_argument('-o', '--offset', default=0, type=int, help='Offset slicer [0, steps].')
   
-  
+  # shape arguments  
+  parser.add_argument('-shape', default="plainRect", type=str,
+                        help="plainRect, plainRound, hex, tri, diamondPoint, (custom?)") 
+  # extra shape
+  parser.add_argument('-base', default=10, type=int, help='Some shapes use base [hex].')
+  parser.add_argument('-top', default=10, type=int, help='Some shapes use top [hex].')
+  parser.add_argument('-point', default=10, type=int, help='Some shapes use point [diamonPoint].')
   
   args = parser.parse_args()
   font_file = args.fontfile
@@ -63,9 +72,13 @@ def run():
   s = args.steps
   a = args.angle
   t = args.thickness
+  o = args.offset
+  ns = args.noSmall
+
   inner = not args.noCounters
   shape = args.shape
 
+  
 
   chars = args.characters
 
@@ -78,7 +91,7 @@ def run():
     
     _p = getPath(ufo[gn])
 
-    q = slicer.getSlicedPathPath(_p, steps=s, thickness=t, angle=a, offset=0, inner=inner, nosmall=t, shape=shape, base=10, top=10, point=-10)
+    q = slicer.getSlicedPathPath(_p, steps=s, thickness=t, angle=a, offset=o, inner=inner, nosmall=ns, shape=shape, base=args.base, top=args.top, point=args.point)
     slicer.writePathInGlyph(q,panfont[gn])
     
   panfont.save("%s/Desktop/pan.ufo" % Path.home())
